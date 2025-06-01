a4655a516a2c43bff2a87bcf18e976e6566a8870
harrissondutra <harrissondutra@gmail.com>
2025-06-01 16:47:29 -0300

    Ajustes do field

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -4,6 +4,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -15,6 +16,7 @@ import { FormsModule } from '@angular/forms';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatIconModule,
     FormsModule
   ],
   templateUrl: './home.html',
